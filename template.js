042bc2a788b4ac9bc16bae228962539c4763172f
Diganta165 <mdsrdiganta@gmail.com>
2021-02-01 16:47:18 +0600

    learned about template string

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+
+const multiLine = `I love you.
+I need you.
+I miss you.
+Nope it's all a lie XD kekw `
+console.log(fullName2);
+console.log(multiLine);
